4f2635486f067be5b9def7bae7e4a90f974eac7b
David Robillard <d@drobilla.net>
2024-08-01 08:13:04 -0400

    Add strict header warning test

---
+++ test/headers/test_header.c
@@ -0,0 +1,13 @@
+// Copyright 2022-2024 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include "sajs/sajs.h" // IWYU pragma: keep
+
+#if defined(__GNUC__)
+__attribute__((const))
+#endif
+int
+main(void)
+{
+  return 0;
+}
